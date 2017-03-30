c1c929912044faf27a1a4958c2f72d74940b70c9
Marcin Drazek <marcindr1@gmail.com>
2017-03-30 22:49:10 +0200

    .

--- bin/mardraze.js
+++ bin/mardraze.js
@@ -5,23 +5,23 @@ if(!shell.which('git')){
     process.exit(1);
 }
 
-let commands = {
+var commands = {
     angular: function(attr1, attr2){
         //shell.exec('git clone https://github.com/mardraze/angular .');
         console.log(attr1, attr2);
     }
 };
 
-let showHelp = function(){
-    let possibleCommands = Object.keys(commands);
+var showHelp = function(){
+    var possibleCommands = Object.keys(commands);
     shell.echo('Usage: mardraze [command]');
     shell.echo('Possible commands: '+possibleCommands.join(', '));
 };
 
 if(process.argv.length > 2){
-    let command = process.argv[2];
+    var command = process.argv[2];
     if(commands.hasOwnProperty(command)){
-        let args = process.argv.slice(2, process.argv.length-2);
+        var args = process.argv.slice(2, process.argv.length-2);
         console.log(process.argv, args);
         commands[command](...args);
     }else{
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 echo "Git message:"
 read -r line
+if [ -z "$line" ]; then
+  line='.'
+fi
+echo "message $line"
 git add --all
 git commit -m"$line"
 npm version patch && npm publish && git push --follow-tags
