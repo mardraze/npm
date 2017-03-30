8918f8c982dd90eeff2b63cfa6be7fd24a105391
Marcin Drazek <marcindr1@gmail.com>
2017-03-30 23:18:43 +0200

    .

--- bin/mardraze.js
+++ bin/mardraze.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 var shell = require('shelljs');
+var fs = require('fs');
+
 if(!shell.which('git')){
     shell.echo('git command is not found');
     process.exit(1);
@@ -10,8 +12,19 @@ var commands = {
         if(!dir){
             dir = 'mardraze-angular';
         }
-        shell.exec('git clone https://github.com/mardraze/angular '+dir);
-        shell.echo('Angular project has been created in "'+dir+'" directory');
+        if (!fs.existsSync(dir)) {
+            shell.exec('git clone https://github.com/mardraze/angular '+dir);
+            shell.echo('Angular project has been created in "'+dir+'" directory');
+            shell.echo('What next?');
+            shell.echo('npm install - install depedencies');
+            shell.echo('grunt - build code for debugging');
+            shell.echo('grunt watch - watch file events, compile the code when file has been changed');
+            shell.echo('grunt dist - minimalize the code');
+            shell.echo('Output files has been created defautly in "out" directory');
+            return true;
+        }else{
+            shell.echo('Directory "'+dir+'" is not empty');
+        }
     }
 };
 
@@ -25,7 +38,10 @@ if(process.argv.length > 2){
     var command = process.argv[2];
     if(commands.hasOwnProperty(command)){
         var args = process.argv.slice(3);
-        commands[command].apply(undefined, args);
+        var success = commands[command].apply(undefined, args);
+        if(success){
+            process.exit(0);
+        }
     }else{
         shell.echo('Unknown command');
         showHelp();
@@ -33,3 +49,4 @@ if(process.argv.length > 2){
 }else{
     showHelp();
 }
+process.exit(1);
