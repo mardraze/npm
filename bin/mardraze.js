cbdebbb70f0ae3c9d223dd6084e78ca781c0e60a
Marcin Drazek <marcindr1@gmail.com>
2017-03-30 22:46:13 +0200

    .

--- bin/mardraze.js
+++ bin/mardraze.js
@@ -4,9 +4,30 @@ if(!shell.which('git')){
     shell.echo('git command is not found');
     process.exit(1);
 }
-if(process.argv.length > 2){
-    switch(process.argv[2]){
-        case "angular":
-            shell.exec('git clone https://github.com/mardraze/angular .');
+
+let commands = {
+    angular: function(attr1, attr2){
+        //shell.exec('git clone https://github.com/mardraze/angular .');
+        console.log(attr1, attr2);
     }
+};
+
+let showHelp = function(){
+    let possibleCommands = Object.keys(commands);
+    shell.echo('Usage: mardraze [command]');
+    shell.echo('Possible commands: '+possibleCommands.join(', '));
+};
+
+if(process.argv.length > 2){
+    let command = process.argv[2];
+    if(commands.hasOwnProperty(command)){
+        let args = process.argv.slice(2, process.argv.length-2);
+        console.log(process.argv, args);
+        commands[command](...args);
+    }else{
+        shell.echo('Unknown command');
+        showHelp();
+    }
+}else{
+    showHelp();
 }
